0302c38f2adf36836eac205fdbdad06df6739797
EmilianoTCS <emi.vega84@gmail.com>
2022-12-01 16:28:44 -0300

    cambios en services para conexion remota

--- src/components/services/LoginService.js
+++ src/components/services/LoginService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  "http://localhost/App_v2/AcademiaFormaci%C3%B3n_V2/login.php?login";
+const baseURL = "https://coebackendacademia.herokuapp.com/login.php?login";
 
 const loginService = async (credentials) => {
   const { data } = await axios.post(baseURL, credentials);
