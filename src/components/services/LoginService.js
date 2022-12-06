4bde1117b6d3b6915fc1070d7e66e2bacab2f328
EmilianoTCS <emi.vega84@gmail.com>
2022-12-06 12:44:39 -0300

    Corrección en sintaxys de ./services

--- src/components/services/LoginService.js
+++ src/components/services/LoginService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  "http://localhost/App_v2/AcademiaFormaci%C3%B3n_V4/login.php?login";
+  "http://localhost/App_v2/AcademiaFormaci%C3%B3n_V2/login.php?login";
 
 const loginService = async (credentials) => {
   const { data } = await axios.post(baseURL, credentials);
--- src/components/services/SendDataService.js
+++ src/components/services/SendDataService.js
@@ -1,5 +1,5 @@
 export default function SendDataService(url, operationUrl, data) {
-  const baseURL = `http://localhost/App_v2/AcademiaFormaci%C3%B3n_V4/${url}?${operationUrl}`;
+  const baseURL = `http://localhost/App_v2/AcademiaFormaci%C3%B3n_V2/${url}?${operationUrl}`;
 
   return fetch(baseURL, {
     method: "post",
